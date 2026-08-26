8d5eeb68278cafe20081dd9a7a7bf685bf2fc0ce
Mohamed Gad <gadm5336@gmail.com>
2026-08-26 10:17:43 -0500

    fix authentication import

--- server/user_app/authentication.py
+++ server/user_app/authentication.py
@@ -1,5 +1,6 @@
-
-from rest_framework_simplejwt import JWTAuthentication
+from rest_framework_simplejwt.authentication import (
+    JWTAuthentication,
+)
 
 
 class JWTCookieAuthentication(JWTAuthentication):
